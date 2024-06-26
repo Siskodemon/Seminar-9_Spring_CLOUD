040fdf0fc9e1e9bf257024c3f32b3ca9814d3c08
Siskodemon <whiteprist@gmail.com>
2024-06-26 15:25:15 +0300

    It's began!

---
+++ src/main/java/com/evilcorp/Spring_Cloud/SpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.evilcorp.Spring_Cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudApplication.class, args);
+	}
+
+}
