97d48edaaf0f4c2337ed6436d46c82ae736d5dfe
iaa <123@gmail.com>
2024-06-27 10:44:05 +0300

    Path adjustments

--- issuer-service/src/main/java/com/evilcorp/api/BookProvider.java
+++ issuer-service/src/main/java/com/evilcorp/api/BookProvider.java
@@ -1,10 +1,16 @@
 package com.evilcorp.api;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 import lombok.Data;
+import org.apache.logging.log4j.CloseableThreadContext;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class BookProvider {
@@ -14,24 +20,32 @@ public class BookProvider {
     // WebClient - spring.reactive (позже разобрать)
 
     private final WebClient webClient;
+    private final EurekaClient eurekaClient;
 
-    public BookProvider() {
-        webClient = WebClient.builder()
-
-
-                .build();
+    public BookProvider(EurekaClient eurekaClient) {
+        webClient = WebClient.builder().build();
+        this.eurekaClient = eurekaClient;
     }
 
     public UUID getRandomBookId(){
         // Цель: Вызвать запрос GET http:\\localhost:8180/api/book/random, получить ID и вернуть.
 
         BookResponse randomBook = webClient.get()
-                .uri("http://localhost:8180/api/book/random")
+                .uri(getBookServiceIP() + "/api/book/random")
                 .retrieve()
                 .bodyToMono(BookResponse.class)
                 .block(); // команда для "блокирования" текущего метода в потоке до его выполнения
 
         return randomBook.getId();
+    }
+
+    private String getBookServiceIP(){
+        Application application = eurekaClient.getApplication("BOOK-SERVICE");
+        List<InstanceInfo> instances = application.getInstances();
+
+        InstanceInfo randomInstance = instances.get(ThreadLocalRandom.current().nextInt(instances.size()));
+        //return "http://" + randomInstance.getIPAddr() + ":" + randomInstance.getPort();
+        return  randomInstance.getHomePageUrl();
     }
 
     @Data
